302bced890d73e4a7264453b7c76912522e634ac
asfm4001 <asfm4001@yahoo.com.tw>
2024-09-09 11:30:32 +0800

    fix: Import password from .env

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
+import os
+# 引入dotenv 將環境變數(env)引用
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config(object):
+    # Database Configuration
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    # Flask Gamil Config
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.getenv("GMAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
